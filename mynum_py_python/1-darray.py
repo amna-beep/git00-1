9eb56c3bf79f45c4537bad7576f6e6064d4bdaa2
amna-beep <amnajabeen45@gmail.com>
2025-11-17 11:49:25 +0500

    myfirstcomit

---
+++ mynum_py_python/1-darray.py
@@ -0,0 +1,9 @@
+import numpy as np 
+a=np.array([5,5,5,])
+print(a)
+categories=type(a)  #type of array
+print(categories)
+length_a=len(a)  #length of array
+print(length_a)
+index=a[0]       #index of aray
+print(index)
